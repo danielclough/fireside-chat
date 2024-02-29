1f3cb7e8603d9954da03d0b09f7a310631c8999c
danielclough <danielclough@users.noreply.github.com>
2024-02-28 18:11:37 -0800

    fix build

--- frontend/build.rs
+++ frontend/build.rs
@@ -4,11 +4,11 @@ pub fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=Cargo.lock");
 
-    // let root_dir: std::path::PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
-    // let generated_dir = root_dir.join("generated");
+    let root_dir: std::path::PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
+    let generated_dir = root_dir.join("generated");
     // let js_dir = generated_dir.join("js");
 
-    // leptonic_theme::generate(generated_dir.join("leptonic"));
+    _ = leptonic_theme::generate(generated_dir.join("leptonic"));
     println!("cargo:warning=theme written");
 
     // std::fs::create_dir_all(js_dir.clone()).unwrap();
