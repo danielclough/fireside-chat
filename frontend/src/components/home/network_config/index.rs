686dd44032f3d5fe74a8ab65a110527b0d474962
danielclough <danielclough@users.noreply.github.com>
2024-03-07 23:00:54 -0800

    fix: adds padding to network buttons

--- frontend/src/components/home/network_config/index.rs
+++ frontend/src/components/home/network_config/index.rs
@@ -50,7 +50,7 @@ pub fn NetworkConfig(
             <strong>"Network Backend: "</strong>
         </P>
         <Show when= move || backend_url_init == localhost>
-            <Button
+            <Button class="network-button"
                 on_click=move |_| {
                     set_backend_url.set(localhost.to_string());
                     reset_if_ready(backend_url, database_url, set_database_error, set_backend_error, set_show_network_init_modal);
@@ -60,7 +60,7 @@ pub fn NetworkConfig(
             </Button>
         </Show>
         <Show when= move || backend_url_init != hosted_backend>
-            <Button
+            <Button class="network-button"
                 on_click=move |_| {
                     set_backend_url.set(hosted_backend.to_string());
                     reset_if_ready(backend_url, database_url, set_database_error, set_backend_error, set_show_network_init_modal);
@@ -71,7 +71,7 @@ pub fn NetworkConfig(
         </Show>
 
         <Show when= move || backend_url_init != hosted_database && backend_url_init != localhost && database_url_init != "">
-            <Button
+            <Button class="network-button"
                 on_click=move |_| {
                     set_database_url.set(backend_url_init.to_string());
                     reset_if_ready(backend_url, database_url, set_database_error, set_backend_error, set_show_network_init_modal);
@@ -96,7 +96,7 @@ pub fn NetworkConfig(
             <strong>"Network Database: "</strong>
         </P>
         <Show when= move || database_url_init == localhost>
-            <Button
+            <Button class="network-button"
                 on_click=move |_| {
                     set_database_url.set(localhost.to_string());
                     reset_if_ready(backend_url, database_url, set_database_error, set_backend_error, set_show_network_init_modal);
@@ -106,7 +106,7 @@ pub fn NetworkConfig(
             </Button>
         </Show>
         <Show when= move || database_url_init != hosted_database>
-            <Button
+            <Button class="network-button"
                 on_click=move |_| {
                     set_database_url.set(hosted_database.to_string());
                     reset_if_ready(backend_url, database_url, set_database_error, set_backend_error, set_show_network_init_modal);
@@ -118,7 +118,7 @@ pub fn NetworkConfig(
 
 
         <Show when= move || database_url_init != hosted_database && database_url_init != localhost && database_url_init != "">
-            <Button
+            <Button class="network-button"
                 on_click=move |_| {
                     set_database_url.set(database_url_init.to_string());
                     reset_if_ready(backend_url, database_url, set_database_error, set_backend_error, set_show_network_init_modal);
